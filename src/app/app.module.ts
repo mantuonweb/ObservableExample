0719223b094fc7ce161d50b0bd09de6afdef2a8d
mantuonweb <mantu.nigam@globallogic.com>
2017-06-10 19:01:13 +0530

    added
    added new feature

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,7 +2,7 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { HttpModule } from '@angular/http';
 import { AppComponent } from './app.component';
-
+//https://angular-2-training-book.rangle.io/handout/observables/summary.html
 @NgModule({
   declarations: [
     AppComponent
