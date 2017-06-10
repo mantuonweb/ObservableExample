d223c6eca5ed39ff0a63054ac3d0d91b76a47d44
mantuonweb <mantu.nigam@globallogic.com>
2017-06-10 18:21:45 +0530

    some minute modification
    Calling .unsubscribe() will unhook a member's callbacks listening in on the Observable stream. When creating an Observable you can also return a custom callback, onUnsubscribe, that will be invoked when a member listening to the stream has unsubscribed. This is useful for any kind of cleanup that must be implemented. If we did not clear the setTimeout then values would still be emitting, but there would be no one listening. To save resources we should stop values from being emitted. An important thing to note is that when you call .unsubscribe() you are destroying the subscription object that is listening, therefore the on-complete event attached to that subscription object will not get called.
    In most cases we will not need to explicitly call the unsubscribe method unless we want to cancel early or our Observable has a longer lifespan than our subscription. The default behavior of Observable operators is to dispose of the subscription as soon as .complete() or .error() messages are published. Keep in mind that RxJS was designed to be used in a "fire and forget" fashion most of the time.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -33,8 +33,25 @@ export class AppComponent {
           setTimeout(() => {
               observer.next(43);
           }, 2000);
+          /**
+          Calling .unsubscribe() will unhook a member's callbacks listening in on the Observable stream. When creating an Observable you can also return a custom callback, onUnsubscribe, that will be invoked when a member listening to the stream has unsubscribed. This is useful for any kind of cleanup that must be implemented. If we did not clear the setTimeout then values would still be emitting, but there would be no one listening. To save resources we should stop values from being emitted. An important thing to note is that when you call .unsubscribe() you are destroying the subscription object that is listening, therefore the on-complete event attached to that subscription object will not get called.
+In most cases we will not need to explicitly call the unsubscribe method unless we want to cancel early or our Observable has a longer lifespan than our subscription. The default behavior of Observable operators is to dispose of the subscription as soon as .complete() or .error() messages are published. Keep in mind that RxJS was designed to be used in a "fire and forget" fashion most of the time.
+          **/
           setTimeout(() => {
               observer.error(new Error('Something bad happened!'));
+          }, 2000);
+
+          setTimeout(() => {
+              observer.next(44);
+          }, 2000);
+          setTimeout(() => {
+              observer.next(45);
+          }, 2000);
+          setTimeout(() => {
+              observer.next(46);
+          }, 2000);
+          setTimeout(() => {
+              observer.next(47);
           }, 2000);
            
           setTimeout(() => {
